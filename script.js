32b602bb3f259a2e66090b34ecea1f3b901ce8be
Sam <samuelkebbell@gmail.com>
2024-10-02 14:17:18 +0100

    Updates

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+
+  setTimeout(() => {
+      document.getElementById('loader').style.display = 'none';
+      document.getElementById('slpp').style.display = 'block';
+  }, 1000);
+});
